4067188a908c678bce5a0a425918f6eef7e4317f
akbar904 <zharfan@walturn.com>
2024-10-15 07:49:25 +0700

    Revert "Modify App: your-nnkew-kkrnnnejjpno-kkn12j3 - Success (#4)"
    This reverts commit 242eaa7ec18ad39677f29004495cb7ef21fe6c77.

--- src/lib/features/home/home_view.dart
+++ src/lib/features/home/home_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:stacked/stacked.dart';
@@ -20,25 +19,53 @@ class HomeView extends StackedView<HomeViewModel> {
           padding: const EdgeInsets.symmetric(horizontal: 25),
           child: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 const Gap(50),
-                const Text(
-                  'User Information',
-                  style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.w900,
-                  ),
+                Column(
+                  children: [
+                    const Text(
+                      'Hello from STEVE x STACKED!',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                    const Gap(25),
+                    MaterialButton(
+                      color: Colors.black,
+                      onPressed: viewModel.incrementCounter,
+                      child: Text(
+                        viewModel.counterLabel,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
                 ),
-                const Gap(25),
-                Text(
-                  'Name: ${viewModel.userName}',
-                  style: const TextStyle(fontSize: 20),
-                ),
-                const Gap(10),
-                Text(
-                  'Email: ${viewModel.userEmail}',
-                  style: const TextStyle(fontSize: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    MaterialButton(
+                      color: Colors.grey,
+                      onPressed: viewModel.showDialog,
+                      child: const Text(
+                        'Show Dialog',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    MaterialButton(
+                      color: Colors.grey,
+                      onPressed: viewModel.showBottomSheet,
+                      child: const Text(
+                        'Show Bottom Sheet',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
