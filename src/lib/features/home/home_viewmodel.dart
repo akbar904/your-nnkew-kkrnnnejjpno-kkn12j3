242eaa7ec18ad39677f29004495cb7ef21fe6c77
akbar904 <zharfan@walturn.com>
2024-10-15 07:48:48 +0700

    Modify App: your-nnkew-kkrnnnejjpno-kkn12j3 - Success (#4)
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files
    
    * Update files

--- src/lib/features/home/home_viewmodel.dart
+++ src/lib/features/home/home_viewmodel.dart
@@ -1,35 +1,43 @@
+
 import 'package:my_app/app/app.bottomsheets.dart';
 import 'package:my_app/app/app.dialogs.dart';
 import 'package:my_app/app/app.locator.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
+class MockUser {
+	final String name;
+	final int age;
+	final String email;
+
+	MockUser({required this.name, required this.age, required this.email});
+}
+
 class HomeViewModel extends BaseViewModel {
-  final _dialogService = locator<DialogService>();
-  final _bottomSheetService = locator<BottomSheetService>();
+	final _dialogService = locator<DialogService>();
+	final _bottomSheetService = locator<BottomSheetService>();
 
-  String get counterLabel => 'Counter is: $_counter';
+	MockUser? _user;
+	MockUser? get user => _user;
 
-  int _counter = 0;
+	void fetchUserData() {
+		_user = MockUser(name: 'John Doe', age: 30, email: 'johndoe@example.com');
+		rebuildUi();
+	}
 
-  void incrementCounter() {
-    _counter++;
-    rebuildUi();
-  }
+	void showDialog() {
+		_dialogService.showCustomDialog(
+			variant: DialogType.infoAlert,
+			title: 'User Info',
+			description: 'Name: ${_user?.name}, Age: ${_user?.age}, Email: ${_user?.email}',
+		);
+	}
 
-  void showDialog() {
-    _dialogService.showCustomDialog(
-      variant: DialogType.infoAlert,
-      title: 'Steve Rocks!',
-      description: 'Give steve $_counter stars on Github',
-    );
-  }
-
-  void showBottomSheet() {
-    _bottomSheetService.showCustomSheet(
-      variant: BottomSheetType.notice,
-      title: 'title',
-      description: 'desc',
-    );
-  }
+	void showBottomSheet() {
+		_bottomSheetService.showCustomSheet(
+			variant: BottomSheetType.notice,
+			title: 'User Details',
+			description: 'Name: ${_user?.name}, Age: ${_user?.age}, Email: ${_user?.email}',
+		);
+	}
 }
