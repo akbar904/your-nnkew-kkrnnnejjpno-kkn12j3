7d687b62bc33064e0ce1b7e9a07bf7c5d737e8b9
akbar904 <zharfan@walturn.com>
2024-10-15 07:41:34 +0700

    Update files

--- src/lib/features/home/home_viewmodel.dart
+++ src/lib/features/home/home_viewmodel.dart
@@ -1,3 +1,4 @@
+
 import 'package:my_app/app/app.bottomsheets.dart';
 import 'package:my_app/app/app.dialogs.dart';
 import 'package:my_app/app/app.locator.dart';
@@ -5,31 +6,33 @@ import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class HomeViewModel extends BaseViewModel {
-  final _dialogService = locator<DialogService>();
-  final _bottomSheetService = locator<BottomSheetService>();
+	final _dialogService = locator<DialogService>();
+	final _bottomSheetService = locator<BottomSheetService>();
 
-  String get counterLabel => 'Counter is: $_counter';
+	List<String> _financialData = [];
 
-  int _counter = 0;
+	List<String> get financialData => _financialData;
 
-  void incrementCounter() {
-    _counter++;
-    rebuildUi();
-  }
+	Future<void> fetchFinancialData() async {
+		// Simulate fetching financial data
+		await Future.delayed(Duration(seconds: 2));
+		_financialData = ['Stock A: \$100', 'Stock B: \$200', 'Stock C: \$300'];
+		rebuildUi();
+	}
 
-  void showDialog() {
-    _dialogService.showCustomDialog(
-      variant: DialogType.infoAlert,
-      title: 'Steve Rocks!',
-      description: 'Give steve $_counter stars on Github',
-    );
-  }
+	void showDialog() {
+		_dialogService.showCustomDialog(
+			variant: DialogType.infoAlert,
+			title: 'Financial Data',
+			description: 'Here is some financial data',
+		);
+	}
 
-  void showBottomSheet() {
-    _bottomSheetService.showCustomSheet(
-      variant: BottomSheetType.notice,
-      title: 'title',
-      description: 'desc',
-    );
-  }
+	void showBottomSheet() {
+		_bottomSheetService.showCustomSheet(
+			variant: BottomSheetType.notice,
+			title: 'Financial Notice',
+			description: 'Financial data has been updated',
+		);
+	}
 }
